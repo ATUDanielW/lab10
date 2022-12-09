e7613643b97e7fe3eb412e7acd64a6a8a9083175
Daniel <G00386720@atu.ie>
2022-12-09 10:15:53 +0000

    lab 9 completed

--- src/components/read.js
+++ src/components/read.js
@@ -2,28 +2,40 @@ import React from "react";
 import { Books } from "./books";
 import axios from "axios";
 
-export class Read extends React.Component{
-    
+export class Read extends React.Component {
+    constructor() {
+        super();
+        this.Reload = this.Reload.bind(this);
+    }
+    Reload() {
+        axios.get('http://localhost:4000/api/books')
+            .then((response) => {
+                this.setState({ books: response.data })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 
     componentDidMount() {
         axios.get('http://localhost:4000/api/books')
-        .then((response)=>{
-            this.setState({books:response.data})
-        })
-        .catch((error)=>{
-            console.log(error);
-        })
+            .then((response) => {
+                this.setState({ books: response.data })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     state = {
-        books:[ ]
+        books: []
     }
-    
-    render(){
-        return(
+
+    render() {
+        return (
             <div>
                 <h3>Hello from my Read component!</h3>
-                <Books books={this.state.books}></Books>
+                <Books books={this.state.books} Reload={this.Reload}></Books>
             </div>
         );
     }
